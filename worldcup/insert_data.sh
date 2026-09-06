21a583995014a68bcde22f486b198e3c5af3137a
Nikodem Karla <nikodemkarla@gmail.com>
2026-09-06 14:11:35 +0200

    Complete worldcup project
    Signed-off-by: Nikodem Karla <nikodemkarla@gmail.com>

---
+++ worldcup/insert_data.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "TRUNCATE TABLE games, teams;")
+
+tail -n +2 games.csv | while IFS="," read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  WINNER_ID=$($PSQL "
+    INSERT INTO teams(name)
+    VALUES('$WINNER')
+    ON CONFLICT(name) DO UPDATE
+    SET name = EXCLUDED.name
+    RETURNING team_id;
+  " | grep -E '^[0-9]+$')
+
+  OPPONENT_ID=$($PSQL "
+    INSERT INTO teams(name)
+    VALUES('$OPPONENT')
+    ON CONFLICT(name) DO UPDATE
+    SET name = EXCLUDED.name
+    RETURNING team_id;
+  " | grep -E '^[0-9]+$')
+
+  echo $($PSQL "
+    INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
+    VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);
+  ")
+done
